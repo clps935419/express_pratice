595088fbbf502ca1d11a401afec1f2f1883b5bb2
Leonard <clps935419@gmail.com>
2023-06-16 00:18:45 +0800

    feat:新增userController

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,9 +1,21 @@
+const User = require("../models/usersModel");
+const { generateSendJWT } = require("../service/auth");
+
 const usersController = {
   async signUp(req, res, next) {
     console.log("測試", req);
-    return {
-      a:11
-    }
+    let { email, password, confirmPassword, name } = req.body;
+    //防呆
+
+
+    // 加密密碼
+    password = await bcrypt.hash(req.body.password, 12);
+    const newUser = await User.create({
+      email,
+      password,
+      name,
+    });
+    generateSendJWT(newUser, 201, res);
   },
 };
 
